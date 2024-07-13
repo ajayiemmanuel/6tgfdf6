7460342ce62e9acfccf95c3b28f49b877630a68e
ajayiemmanuel <86613616+ajayiemmanuel@users.noreply.github.com>
2024-07-14 00:44:43 +0100

    Delete assets directory

--- assets/js/init-aos.js
+++
@@ -1,8 +0,0 @@
-/*=================================
-    init aos CSS start
-===================================*/
-AOS.init({
-    once: true,
-    offset: 20,
-});
-window.addEventListener("load", AOS.refresh);
